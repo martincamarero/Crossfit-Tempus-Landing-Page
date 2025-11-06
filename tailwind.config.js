6677002ad93e88dcee65dcd75f397d87c0735481
martincamarero <martinprograma02@gmail.com>
2025-11-06 15:44:11 +0100

    15:44

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,24 @@
 // tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        "./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}",
-        "./public/**/*.html",
-    ],
-    theme: {
-        extend: {
-            colors: {
-                primary: "#023c72",
-                light: "#1b78bd",
-                // Puedes añadir otros colores aquí si los usas
-            },
-            screens: {
-                // Aquí defines tus breakpoints personalizados si lo deseas
-                xs: "370px",
-                sm: "640px",
-                md: "768px",
-                lg: "1024px",
-                xl: "1280px",
-                "2xl": "1536px",
-            },
-        },
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}', './public/**/*.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#023c72',
+        light: '#1b78bd',
+        // Puedes añadir otros colores aquí si los usas
+      },
+      screens: {
+        // Aquí defines tus breakpoints personalizados si lo deseas
+        xs: '370px',
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1536px',
+      },
     },
-    plugins: [],
-};
+  },
+  plugins: [],
+}
